e5e1b8fef705f63be45dbd2f94f34e0f8a86ad06
Petar Bursakj <petar@thrasker.com>
2024-06-18 20:41:09 +0200

    v1.2

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import shuffle from './utilities/shuffle';
 import Card from './components/Card';
+import Header from "./components/Header";
 
 function App() {
   const [cards, setCards] = useState(shuffle);
@@ -21,8 +22,55 @@ function App() {
     setDisabled(false);
   };
 
+  const handleNewGame = () => {
+    setWins(0);
+    handleTurn();
+    setCards(shuffle);
+  };
+
+  useEffect(() => {
+    let pickTimer;
+
+    if (pickOne && pickTwo) {
+      if (pickOne.image === pickTwo.image) {
+        setCards((prevCards) => {
+          return prevCards.map((card) => {
+            if (card.image === pickOne.image) {
+              return { ...card, matched: true };
+            } else {
+              return card;
+            }
+          });
+        });
+        handleTurn();
+      } else {
+        setDisabled(true);
+        pickTimer = setTimeout(() => {
+          handleTurn();
+        }, 1000);
+      }
+    }
+
+    return () => {
+      clearTimeout(pickTimer);
+    };
+  }, [cards, pickOne, pickTwo]);
+
+  useEffect(() => {
+    const checkWins = cards.filter((card) => !card.matched);
+
+    if (cards.langht && checkWins.langht < 1) {
+      console.log("You win");
+      setWins(wins + 1);
+      handleTurn();
+      setCards(shuffle);
+    }
+  }, [cards, wins]);
+
   return (
     <>
+    
+      <Header handleNewGame={handleNewGame} wins={wins} />
     
       <div className='grid'>
 
@@ -33,8 +81,8 @@ function App() {
             <Card
               key={id}
               image={image}
-              selected={false}
-              onClick={() => {}}
+              selected={card === pickOne || card === pickTwo || matched}
+              onClick={() => handleClick(card)}
             />
           );
 
